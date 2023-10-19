e6362d72ee7f7ca1d86fbf0fe6fcd29dd8f987ea
Pham Luc <lucpham566@gmail.com>
2023-10-19 11:00:07 +0700

    add PlayerEquipmentManager

---
+++ Player/PlayerEquipmentManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerEquipmentManager : MonoBehaviour
+{
+    public List<EquipmentClass> equipmentList;
+    public PlayerCharacter playerCharacter;
+    public GearEquipper gearEquipper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCharacter = GetComponent<PlayerCharacter>();
+        gearEquipper = GetComponent<GearEquipper>();
+    }
+    public void EquipItem(EquipmentClass item)
+    {
+        bool itemTypeExists = false;
+
+        for (int i = 0; i < equipmentList.Count; i++)
+        {
+            if (equipmentList[i].itemType == item.itemType)
+            {
+                // Nếu đã tồn tại, thay thế nó bằng item mới
+                equipmentList[i] = item;
+                itemTypeExists = true;
+                break;
+            }
+        }
+
+        if (!itemTypeExists)
+        {
+            // Nếu itemType chưa tồn tại, thêm item vào danh sách
+            equipmentList.Add(item);
+        }
+
+        UpdateProperties();
+
+    }
+
+    public void UnequipItem(EquipmentClass item)
+    {
+        // Tìm và xóa itemType ra khỏi danh sách
+        for (int i = 0; i < equipmentList.Count; i++)
+        {
+            if (equipmentList[i].itemType == item.itemType)
+            {
+                equipmentList.RemoveAt(i);
+                break;
+            }
+        }
+
+        UpdateProperties();
+
+        // Cập nhật các thuộc tính của nhân vật sau khi tháo item ra khỏi trang bị.
+    }
+
+        private void UpdateProperties()
+    {
+        playerCharacter.AddEquipmentStats();
+        gearEquipper.LoadSkin();
+    }
+}
